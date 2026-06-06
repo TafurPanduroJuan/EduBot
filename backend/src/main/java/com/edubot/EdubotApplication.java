7877ffcdf3a499e47de04fdd415183c521f96b7d
TafurPanduroJuan <u23230022@utp.edu.pe>
2026-06-06 14:00:17 -0500

    add @EnableScheduling annotation

--- backend/src/main/java/com/edubot/EdubotApplication.java
+++ backend/src/main/java/com/edubot/EdubotApplication.java
@@ -2,8 +2,10 @@ package com.edubot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EdubotApplication {
     public static void main(String[] args) {
         SpringApplication.run(EdubotApplication.class, args);
